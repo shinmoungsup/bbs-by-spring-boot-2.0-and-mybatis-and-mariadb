8f564800d9ab5c593240770e62ab9cbbabedeaa3
shinmoungsup <noodu0529@gmail.com>
2020-07-31 16:24:58 +0900

    회원가입 처리하고, 회원가입이 완료되면 로그인 페이지로 보내기 작업

--- src/main/java/com/sbs/starter/service/MemberService.java
+++ src/main/java/com/sbs/starter/service/MemberService.java
@@ -1,7 +1,8 @@
 package com.sbs.starter.service;
-
 import java.util.Map;
 
 public interface MemberService {
 	public Map<String, Object> checkLoginIdDup(String loginId);
+
+	public Map<String, Object> join(Map<String, Object> param);
 }
